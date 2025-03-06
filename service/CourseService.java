043ef23d5dabed00041c8dc33518e9d40d7b8749
Araz05 <valishovaraz@gmail.com>
2025-03-06 11:02:42 +0400

    addd CourseService.java class

--- service/CourseService.java
+++ service/CourseService.java
@@ -1,5 +1,60 @@
 package service;
 
-public class CourseService {
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Course;
+import model.person.Faculty;
+import model.person.Student;
+
+public class CourseService extends Course {
+
+    public CourseService(String title, String description, int courseCode, int credits, int CRN, char passingGrade,
+            ArrayList<Course> prerequisites, ArrayList<Student> students, Faculty teacher) {
+        super(title, description, courseCode, credits, CRN, passingGrade, prerequisites, students, teacher);
+
+    }
+    private List<Course> courses = new ArrayList<>();
+
+    // public void addStudent(Course course, Student student) {
+    //     course.getStudents().add(student);
+    // }
+
+    public List<Course> getAllCourses() {
+        return courses;
+    }
+
+    public Course getCourseByCRN(int CRN) {
+        for (Course course : courses) {
+            if (course.getCRN() == CRN) {
+                return course;
+            }
+        }
+        System.out.println("Course with CRN " + CRN + " not found.");
+        return null;
+    }
+
+
+
     
+    public void addStudentToCourse(int CRN, Student student) {
+        Course course = getCourseByCRN(CRN);
+        if (course != null) {
+            course.getStudents().add(student);
+            System.out.println("Student added to course: " + course.getTitle());
+        }
+    }
+    public void deleteCourse(int CRN) {
+        Course course = getCourseByCRN(CRN);
+        if (course != null) {
+            courses.remove(course);
+            System.out.println("Course with CRN " + CRN + " deleted.");
+        }
+    }
+
+
+
+    // public void removeStudent(Course course, Student student) {
+    //     course.getStudents().remove(student);
+    // }
 }
